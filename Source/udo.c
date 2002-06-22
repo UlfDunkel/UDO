315d03007e5a3e8ed8dff2d21eab09f694f29df8
Thorsten Otto <12425871+th-otto@users.noreply.github.com>
2002-06-22 09:58:21 +0000

    fix bug in printing propfontsize

--- Source/udo.c
+++ Source/udo.c
@@ -2281,7 +2281,7 @@ LOCAL void c_heading ( void )
 			align[0]= EOS;
 			if (inside_center)	strcpy(align, "\\qc");
 			if (inside_right)	strcpy(align, "\\qr");
-			voutlnf("{%s\\fs%d\\b %s}\\par\\pard\\par", iDocPropfontSize + 14, align, name);
+			voutlnf("{%s\\fs%d\\b %s}\\par\\pard\\par", align, iDocPropfontSize + 14, name);
 			break;
 		case TOWIN:
 		case TOWH4:
@@ -2290,7 +2290,7 @@ LOCAL void c_heading ( void )
 			align[0]= EOS;
 			if (inside_center)	strcpy(align, "\\qc");
 			if (inside_right)	strcpy(align, "\\qr");
-			voutlnf("{%s\\fs%d\\b %s}\\par\\pard\\par", iDocPropfontSize + 14, align, name);
+			voutlnf("{%s\\fs%d\\b %s}\\par\\pard\\par", align, iDocPropfontSize + 14, name);
 			break;
 		case TOKPS:
 			outln("newline");
