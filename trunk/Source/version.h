a76d522ebec75af76c35250119dc8d35ca7b9b41
voja <voja@f85f52be-9311-47d4-a1e1-77e140e2cb11>
2003-12-01 18:38:29 +0000

    Set version to 6.3.3

--- trunk/Source/version.h
+++ trunk/Source/version.h
@@ -43,8 +43,8 @@
 	Release-Nummer und Patchlevel setzen
 	------------------------------------------------------------	*/
 #define	UDO_REL		"6"
-#define UDO_SUBVER      "3"
-#define	UDO_PL		"2"
+#define UDO_SUBVER	"3"
+#define	UDO_PL		"3"
 #define	UDO_URL		"http://www.udo-open-source.org"
 #define	UDO_MADE	"Made with UDO"
 
