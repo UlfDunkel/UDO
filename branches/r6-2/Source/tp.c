fee17041f3e54e624083e0e982d9079d776a659b
Norbert_Hanz <Norbert_Hanz@f85f52be-9311-47d4-a1e1-77e140e2cb11>
2003-12-30 16:02:41 +0000

    Bug #0000040 fixed; Default paper layout was not set

--- branches/r6-2/Source/tp.c
+++ branches/r6-2/Source/tp.c
@@ -75,6 +75,9 @@ LOCAL void free_titdat ( char **var );
 	--------------------------------------------------------------	*/
 GLOBAL BOOLEAN set_mainlayout ( void )
 {
+	/* Fixed Bug #0000040 in r6.2pl2 [NHz] */
+	init_docinfo_data("A4PORTRAIT", &(laydat.paper), FALSE);
+
 	init_docinfo_data("Times New Roman", &(laydat.propfontname), FALSE);
 	laydat.propfontsize = 11;
 	init_docinfo_data("Courier New", &(laydat.monofontname), FALSE);
