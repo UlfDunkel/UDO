1b1df11448af484f080ec704f6102043988acdee
Ulrich_Kretschmer <Ulrich_Kretschmer@f85f52be-9311-47d4-a1e1-77e140e2cb11>
2004-01-14 22:31:37 +0000

    falschen Pfad korrigiert

--- trunk/doc/manual/source/mandehtm.bat
+++ trunk/doc/manual/source/mandehtm.bat
@@ -1,6 +1,6 @@
 @rem dieses Batchfile fuer DOS konvertiert das UDO-Handbuch in Deutsch nach HTML
 @rem this batch file for DOS converts the german UDO manual to HTML
-udo --html --verbose -D LANG_DE --tree -o d:\udotest\index.html manual.u
+udo --html --verbose -D LANG_DE --tree -o ../html-de/index.html manual.u
 copy ..\html-de\index.ulh ..\manual-de.ulh
 copy ..\html-de\index.uth ..\manual-de.uth
 del ..\html-de\index.ulh
