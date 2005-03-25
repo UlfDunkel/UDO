f3dbe2d1288754a0c66a7f978132e1f7ee2221ac
Norbert_Hanz <Norbert_Hanz@f85f52be-9311-47d4-a1e1-77e140e2cb11>
2005-03-25 15:55:09 +0000

    Set up all copyright messages to 2005

--- trunk/Source/version.h
+++ trunk/Source/version.h
@@ -51,8 +51,8 @@
 /*	------------------------------------------------------------
 	Copyright Meldung
 	------------------------------------------------------------	*/
-#define	COPYRIGHT	"Copyright (c) 1995-2001 by Dirk Hagedorn\nCopyright (c) 2001-2004 by The UDO community"
-#define UDO_COPYRIGHT_TEXT "Copyright (!copyright) 1995-2001 by Dirk Hagedorn\t(!nl)\nCopyright (!copyright) 2001-2004 by The UDO community\t(!nl)\n"
+#define	COPYRIGHT	"Copyright (c) 1995-2001 by Dirk Hagedorn\nCopyright (c) 2001-2005 by The UDO community"
+#define UDO_COPYRIGHT_TEXT "Copyright (!copyright) 1995-2001 by Dirk Hagedorn\t(!nl)\nCopyright (!copyright) 2001-2005 by The UDO community\t(!nl)\n"
 
 /*	------------------------------------------------------------
 	Zeichensaetze
