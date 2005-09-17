322803215ffe7a69e2c351943664d66f583fd3e6
Ulrich_Kretschmer <Ulrich_Kretschmer@f85f52be-9311-47d4-a1e1-77e140e2cb11>
2005-09-17 09:19:31 +0000

    aktualisiert

--- doc/manual/source/create_manual_en_html.cmd
+++ doc/manual/source/create_manual_en_html.cmd
@@ -1,5 +1,5 @@
-@rem dieses Batchfile fuer Win32 + OS/2 konvertiert das englische UDO-Handbuch nach HTML
-@rem this batch file for Win32 + OS/2 converts the english UDO manual to HTML
+@REM dieses Batchfile fuer Win32 + OS/2 konvertiert das englische UDO-Handbuch nach HTML
+@REM this batch file for Win32 + OS/2 converts the english UDO manual to HTML
 udo --html --verbose --force-long -D LANG_EN --tree -o ../html-en/index.html ./manual.u %1 %2 %3 %4 %5 %6 %8 %8 %9
 copy ..\html-en\index.ulh ..\manual-en.ulh
 copy ..\html-en\index.uth ..\manual-en.uth
