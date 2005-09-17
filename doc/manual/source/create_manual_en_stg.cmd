54d938fb7a44dd263fc2312f173139d8a8371305
Ulrich_Kretschmer <Ulrich_Kretschmer@f85f52be-9311-47d4-a1e1-77e140e2cb11>
2005-09-17 09:23:03 +0000

    neu

---
+++ doc/manual/source/create_manual_en_stg.cmd
@@ -0,0 +1,3 @@
+@REM dieses Batchfile fuer Win32 + OS/2 konvertiert das englische UDO-Handbuch nach ST-Guide
+@REM this batchfile for Win32 + OS/2 converts the english UDO Manual to ST-Guide
+udo --stg --verbose --force-long -D LANG_EN manual.u -o manual-en.stg %1 %2 %3 %4 %5 %6 %7 %7 %8
