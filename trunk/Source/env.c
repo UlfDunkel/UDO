39b11e769575676f37dc092339a8106d5e50ae4a
UlfDunkel <UlfDunkel@f85f52be-9311-47d4-a1e1-77e140e2cb11>
2006-09-14 15:51:39 +0000

    HTML formatting: more adjustments

--- trunk/Source/env.c
+++ trunk/Source/env.c
@@ -2235,11 +2235,11 @@ GLOBAL void c_item ( void )
 						}
 						c_internal_styles(sBig);
 			 			strinsert(sBig, "<dt>");
-						um_strcat(sBig, "</dt>\n<dd><p>", 1024, "c_item[21]");
+						um_strcat(sBig, "</dt>\n<dd>", 1024, "c_item[21]");
 					}
 					else
 					{
-               	um_strcpy(sBig, "<dt>&nbsp;</dt>\n<dd><p>", 1024, "c_item[22]");
+               	um_strcpy(sBig, "<dt>&nbsp;</dt>\n<dd>", 1024, "c_item[22]");
 					}
 					break;
 
