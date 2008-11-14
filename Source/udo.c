0f13dbe2977a9b81527c2bc67772df16d6db225f
ulf_svn <6141297+UlfDunkel@users.noreply.github.com>
2008-11-14 09:51:50 +0000

    TOPOL introduced

--- Source/udo.c
+++ Source/udo.c
@@ -1,32 +1,69 @@
-/*	##############################################################
-	# @(#) udo.c
-	# @(#)
-	# @(#) Copyright (c) 1995-2001 by Dirk Hagedorn
-	# @(#) Dirk Hagedorn (udo@dirk-hagedorn.de)
-	#
-	# This program is free software; you can redistribute it and/or
-	# modify it under the terms of the GNU General Public License
-	# as published by the Free Software Foundation; either version 2
-	# of the License, or (at your option) any later version.
-	# 
-	# This program is distributed in the hope that it will be useful,
-	# but WITHOUT ANY WARRANTY; without even the implied warranty of
-	# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	# GNU General Public License for more details.
-	# 
-	# You should have received a copy of the GNU General Public License
-	# along with this program; if not, write to the Free Software
-	# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-	# 
-	##############################################################
-	# Start:	Januar 1995
-	# Version:	2. Oktober 2001
-	##############################################################	*/
+/*******************************************************************************
+*
+*  Project name : UDO
+*  Module name  : udo.c
+*  Symbol prefix: udo
+*
+*  Copyright    : 1995-2001 Dirk Hagedorn
+*  Open Source  : since 2001
+*
+*                 This program is free software; you can redistribute it and/or
+*                 modify it under the terms of the GNU General Public License
+*                 as published by the Free Software Foundation; either version 2
+*                 of the License, or (at your option) any later version.
+*                 
+*                 This program is distributed in the hope that it will be useful,
+*                 but WITHOUT ANY WARRANTY; without even the implied warranty of
+*                 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*                 GNU General Public License for more details.
+*                 
+*                 You should have received a copy of the GNU General Public License
+*                 along with this program; if not, write to the Free Software
+*                 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*
+*-------------------------------------------------------------------------------
+*
+*  Author       : Dirk Hagedorn (udo@dirk-hagedorn.de)
+*  Co-Authors   : Ulf Dunkel (fd)
+*  Write access : fd
+*
+*  Notes        : Please add yourself as co-author when you change this file.
+*
+*-------------------------------------------------------------------------------
+*  Things to do : re-write UDO string and encoding engine for full Unicode support 
+*
+*-------------------------------------------------------------------------------
+*  History:
+*
+*  1995:
+*    DH  Jan xx: introduced
+*  2001:
+*    DH  Oct 02: last DH version
+*  2008:
+*    fd  Nov 14: TOPOL introduced
+*
+******************************************|************************************/
+
+/*******************************************************************************
+*
+*     CONSTANTS
+*
+******************************************|************************************/
 
 #ifndef ID_UDO_C
 #define ID_UDO_C
 const char *id_udo_c= "@(#) udo.c       $Date$";
 #endif
+
+
+
+
+
+/*******************************************************************************
+*
+*     INCLUDE FILES
+*
+******************************************|************************************/
 
 #include "import.h"
 #include <stdio.h>
@@ -531,7 +568,7 @@ typedef struct _udolanguage		/* ---- Sprachentabelle ----	*/
 }	UDOLANGUAGE;
 
 
-#define MAXLANGUAGE     13
+#define MAXLANGUAGE     14
 
 LOCAL const UDOLANGUAGE udolanguage[MAXLANGUAGE]=
 {
@@ -545,6 +582,7 @@ LOCAL const UDOLANGUAGE udolanguage[MAXLANGUAGE]=
    {"italian",    TOITA},
    {"latvian",    TOLVA},
    {"norwegian",  TONOR},
+   {"polish",     TOPOL},
    {"portuguese", TOPOR},
    {"spanish",    TOSPA},
    {"swedish",    TOSWE},
