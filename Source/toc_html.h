b3401e71d5b4f327f9376518db9fd1e2eff8cf99
ulf_svn <6141297+UlfDunkel@users.noreply.github.com>
2010-01-23 14:55:54 +0000

    file reformatted *

--- Source/toc_html.h
+++ Source/toc_html.h
@@ -1,32 +1,54 @@
-/* ############################################################
-   # @(#) toc_html.h
-   # @(#)
-   # @(#) Copyright (c) 1995-2001 by Dirk Hagedorn
-   # @(#) Dirk Hagedorn (udo@dirk-hagedorn.de)
-   #
-   # This program is free software; you can redistribute it and/or
-   # modify it under the terms of the GNU General Public License
-   # as published by the Free Software Foundation; either version 2
-   # of the License, or (at your option) any later version.
-   # 
-   # This program is distributed in the hope that it will be useful,
-   # but WITHOUT ANY WARRANTY; without even the implied warranty of
-   # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.   See the
-   # GNU General Public License for more details.
-   # 
-   # You should have received a copy of the GNU General Public License
-   # along with this program; if not, write to the Free Software
-   # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-   #
-   ############################################################ */
+/*******************************************************************************
+*
+*  Project name : UDO
+*  Module name  : toc_html.h
+*  Symbol prefix: -
+*
+*  Copyright    : 1995-2001 Dirk Hagedorn
+*  Open Source  : since 2001
+*
+*                 This program is free software; you can redistribute it and/or
+*                 modify it under the terms of the GNU General Public License
+*                 as published by the Free Software Foundation; either version 2
+*                 of the License, or (at your option) any later version.
+*                 
+*                 This program is distributed in the hope that it will be useful,
+*                 but WITHOUT ANY WARRANTY; without even the implied warranty of
+*                 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*                 GNU General Public License for more details.
+*                 
+*                 You should have received a copy of the GNU General Public License
+*                 along with this program; if not, write to the Free Software
+*                 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*
+*  Description  : -
+*
+*-------------------------------------------------------------------------------
+*
+*  Author       : Dirk Hagedorn (udo@dirk-hagedorn.de)
+*  Co-Authors   : Ulf Dunkel (fd)
+*  Write access : fd
+*
+*  Notes        : Please add yourself as co-author when you change this file.
+*
+*-------------------------------------------------------------------------------
+*  Things to do : -
+*
+*-------------------------------------------------------------------------------
+*  History:
+*
+*  2010:
+*    fd  Jan 23: file reformatted
+*
+******************************************|************************************/
 
+/*******************************************************************************
+*
+*     GLOBAL PROTOTYPES
+*
+******************************************|************************************/
 
-/* ############################################################
-   # globale Funktionen
-   ############################################################ */
-
+   /* Creates the navigation bar for HTML output. */
 GLOBAL BOOLEAN set_html_navigation(void);
 
-/* ############################################################
-   # toc_html.h
-   ############################################################ */
+/* +++ EOF +++ */
