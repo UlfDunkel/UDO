f74f1b1ca00551e5f13403a4da1814d92fdc12ae
ulf_svn <6141297+UlfDunkel@users.noreply.github.com>
2010-02-12 10:43:20 +0000

    file reformatted and tidied up, TAB-free

--- Source/portab.h
+++ Source/portab.h
@@ -1,118 +1,143 @@
-/*	##############################################################
-	# @(#) portab.h
-	# @(#)
-	# @(#) Copyright (c) 1995-2001 by Dirk Hagedorn
-	# @(#) Dirk Hagedorn (udo@dirk-hagedorn.de)
-	#
-	# This program is free software; you can redistribute it and/or
-	# modify it under the terms of the GNU General Public License
-	# as published by the Free Software Foundation; either version 2
-	# of the License, or (at your option) any later version.
-	# 
-	# This program is distributed in the hope that it will be useful,
-	# but WITHOUT ANY WARRANTY; without even the implied warranty of
-	# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	# GNU General Public License for more details.
-	# 
-	# You should have received a copy of the GNU General Public License
-	# along with this program; if not, write to the Free Software
-	# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-	#
-	##############################################################	*/
+/**(TAB=0)**********************************************************************
+*
+*  Project name : UDO
+*  Module name  : portab.h
+*  Symbol prefix: -
+*
+*  Description  : Global portability definitions for several supported platforms.
+*
+*  Copyright    : 1995-2001 Dirk Hagedorn
+*  Open Source  : since 2001
+*
+*                 This program is free software; you can redistribute it and/or
+*                 modify it under the terms of the GNU General Public License
+*                 as published by the Free Software Foundation; either version 2
+*                 of the License, or (at your option) any later version.
+*                 
+*                 This program is distributed in the hope that it will be useful,
+*                 but WITHOUT ANY WARRANTY; without even the implied warranty of
+*                 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*                 GNU General Public License for more details.
+*                 
+*                 You should have received a copy of the GNU General Public License
+*                 along with this program; if not, write to the Free Software
+*                 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*
+*-------------------------------------------------------------------------------
+*
+*  Author       : Dirk Hagedorn (udo@dirk-hagedorn.de)
+*  Co-Authors   : Ulf Dunkel (fd), Gerhard Stoll (ggs)
+*  Write access : fd, ggs
+*
+*  Notes        : Please add yourself as co-author when you change this file.
+*
+*-------------------------------------------------------------------------------
+*  Things to do : re-write UDO string and encoding engine for full Unicode support 
+*
+*-------------------------------------------------------------------------------
+*  History:
+*
+*  2010:
+*    fd  Feb 11: file reformatted and tidied up, TAB-free
+*
+******************************************|************************************/
 
 #ifndef PORTAB_H
-#define	PORTAB_H
+#define PORTAB_H
 
-/*	--------------------------------------------------------------	*/
+
+
+
+
+/*******************************************************************************
+*
+*     MACRO DEFINITIONS
+*
+******************************************|************************************/
 
 #ifndef TRUE
-#define	TRUE	1
-#define	FALSE	0
+#define TRUE   1
+#define FALSE  0
 #endif
 
-#define	BOOLEAN	int
-#define	UCHAR	unsigned char
-#define	UINT	unsigned int
+#define BOOLEAN    int
+#define UCHAR      unsigned char
+#define UINT       unsigned int
 
-#define	EOS		'\0'
+#define EOS        '\0'
 
-#define	LOCAL	static
-
-
-/*	--------------------------------------------------------------	*/
-
-#ifdef __MSDOS__
-#endif
-
-/*	--------------------------------------------------------------	*/
-
-#ifdef __WIN32__
-#endif
-
-/*	--------------------------------------------------------------	*/
-
-#ifdef __TOS__
-#endif
-
-/*	--------------------------------------------------------------	*/
-
-#ifdef __LINUX__
-#endif
-
-/*	--------------------------------------------------------------	*/
-
-#ifdef __SUNOS__
-#endif
-
-/*	--------------------------------------------------------------	*/
-
-#ifdef __NEXTSTEP__
-#endif
-
-/*	--------------------------------------------------------------	*/
-
-#ifdef __HPUX_ISO__
-#endif
-
-/*	--------------------------------------------------------------	*/
-
-#ifdef __HPUX_ROMAN8__
-#endif
-
-/*	--------------------------------------------------------------	*/
-
-#ifdef __AMIGA__
-#endif
-
-/*	--------------------------------------------------------------	*/
-
-#ifdef __SINIX__
-#endif
-
-/*	--------------------------------------------------------------	*/
-
-#ifdef __BEOS__
-#endif
-
-/*	--------------------------------------------------------------	*/
-
-#ifdef __MACOS__
-#define __AddLFToNL__ 0	/* solange es keinen HCP auf dem Mac gibt notwendig */
-#endif
-
-/*	--------------------------------------------------------------	*/
-
-#ifdef __MACOSX__
-#endif
-
-/*	--------------------------------------------------------------	*/
+#define LOCAL      static
 
 #ifndef UNUSED
-#define UNUSED(x) if (x) {}
+#define UNUSED(x)  x = x                  /* tell compiler that variable seems to be used */
 #endif
+
+
+
+
+
+/*******************************************************************************
+*
+*     COMPILER MACRO DEFINITIONS
+*
+******************************************|************************************/
 
 #ifndef __GNUC__
 #  define __attribute__(x)
 #endif
 
-#endif	/* PORTAB_H */
+
+
+
+
+/*******************************************************************************
+*
+*     OS MACRO DEFINITIONS
+*
+******************************************|************************************/
+
+#ifdef __MSDOS__
+#endif
+
+#ifdef __WIN32__
+#endif
+
+#ifdef __TOS__
+#endif
+
+#ifdef __LINUX__
+#endif
+
+#ifdef __SUNOS__
+#endif
+
+#ifdef __NEXTSTEP__
+#endif
+
+#ifdef __HPUX_ISO__
+#endif
+
+#ifdef __HPUX_ROMAN8__
+#endif
+
+#ifdef __AMIGA__
+#endif
+
+#ifdef __SINIX__
+#endif
+
+#ifdef __BEOS__
+#endif
+
+#ifdef __MACOS__
+#define __AddLFToNL__ 0                   /* solange es keinen HCP auf dem Mac gibt notwendig */
+#endif
+
+#ifdef __MACOSX__
+#endif
+
+#endif   /* PORTAB_H */
+
+
+/* +++ EOF +++ */
+
