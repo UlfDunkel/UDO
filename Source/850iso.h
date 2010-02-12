ee2afef31acf272fe3058d3291d9c037273de3ed
ulf_svn <6141297+UlfDunkel@users.noreply.github.com>
2010-02-12 10:31:50 +0000

    header updated, file reformatted and tidied up (TAB-free)

--- Source/850iso.h
+++ Source/850iso.h
@@ -1,176 +1,200 @@
-/*	############################################################
-	# @(#) 850iso.h
-	# @(#)
-	# @(#) Copyright (c) 1995-2001 by Dirk Hagedorn
-	# @(#) Dirk Hagedorn (udo@dirk-hagedorn.de)
-	#
-	# This program is free software; you can redistribute it and/or
-	# modify it under the terms of the GNU General Public License
-	# as published by the Free Software Foundation; either version 2
-	# of the License, or (at your option) any later version.
-	# 
-	# This program is distributed in the hope that it will be useful,
-	# but WITHOUT ANY WARRANTY; without even the implied warranty of
-	# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	# GNU General Public License for more details.
-	# 
-	# You should have received a copy of the GNU General Public License
-	# along with this program; if not, write to the Free Software
-	# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-	# 
-	# @(#) Uebersetzungstabellen von Codepage 850 nach ISO-Latin-1
-	# basierend auf chr_850.h
-	############################################################	*/
+/**(TAB=0)**********************************************************************
+*
+*  Project name : UDO
+*  Module name  : 850iso.h
+*  Symbol prefix: chr
+*
+*  Description  : Uebersetzungstabellen von Codepage 850 nach ISO-Latin-1
+*                 basierend auf chr_850.h
+*
+*  Copyright    : 1995-2001 Dirk Hagedorn
+*  Open Source  : since 2001
+*
+*                 This program is free software; you can redistribute it and/or
+*                 modify it under the terms of the GNU General Public License
+*                 as published by the Free Software Foundation; either version 2
+*                 of the License, or (at your option) any later version.
+*                 
+*                 This program is distributed in the hope that it will be useful,
+*                 but WITHOUT ANY WARRANTY; without even the implied warranty of
+*                 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*                 GNU General Public License for more details.
+*                 
+*                 You should have received a copy of the GNU General Public License
+*                 along with this program; if not, write to the Free Software
+*                 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*
+*-------------------------------------------------------------------------------
+*
+*  Author       : Dirk Hagedorn (udo@dirk-hagedorn.de)
+*  Co-Authors   : Ulf Dunkel (fd), Gerhard Stoll (ggs)
+*  Write access : fd, ggs
+*
+*  Notes        : Please add yourself as co-author when you change this file.
+*
+*-------------------------------------------------------------------------------
+*  Things to do : re-write UDO string and encoding engine for full Unicode support 
+*
+*-------------------------------------------------------------------------------
+*  History:
+*
+*  2010:
+*    fd  Feb 12: header updated, file reformatted and tidied up (TAB-free)
+*
+******************************************|************************************/
 
-#ifndef	__850ISO__
-#define	__850ISO__
+#ifndef __850ISO__
+#define __850ISO__
 
 
 #ifndef ID_850ISO_H
-#define	ID_850ISO_H
-const char *id_850iso_h= "@(#) 850iso.h    14.04.1998";
+#define ID_850ISO_H
+const char  *id_850iso_h = "@(#) 850iso.h    $date$";
 #endif
 
 
 typedef struct _cp850iso
 {
-	unsigned char	cp850;
-	unsigned char	iso;
-}	CP850ISO;
+   unsigned char   cp850;                 /* */
+   unsigned char   iso;                   /* */
+}  CP850ISO;
 
 
-LOCAL const CP850ISO cp850iso_item[128]=
+LOCAL const CP850ISO   cp850iso_item[128] =
 {
-	{ 128,	199	},		/* ,C		*/
-	{ 129,	252	},		/* "u		*/
-	{ 130,	233	},		/* 'e		*/
-	{ 131,	226	},		/* ^a		*/
-	{ 132,	228	},		/* "a		*/
-	{ 133,	224	},		/* `a		*/
-	{ 134,	229	},		/* .a		*/
-	{ 135,	231	},		/* ,c		*/
-	{ 136,	234	},		/* ^e		*/
-	{ 137,	235	},		/* "e		*/
-	{ 138,	232	},		/* `e		*/
-	{ 139,	239	},		/* "i		*/
-	{ 140,	238	},		/* ^i		*/
-	{ 141,	236	},		/* `i		*/
-	{ 142,	196	},		/* "A		*/
-	{ 143,	197	},		/* .A		*/
-	{ 144,	201	},		/* 'E		*/
-	{ 145,	230	},		/* &ae		*/
-	{ 146,	198	},		/* &AE		*/
-	{ 147,	244	},		/* ^o		*/
-	{ 148,	246	},		/* "o		*/
-	{ 149,	242	},		/* `o		*/
-	{ 150,	251	},		/* ^u		*/
-	{ 151,	249	},		/* `u		*/
-	{ 152,	255	},		/* "y		*/
-	{ 153,	214	},		/* "O		*/
-	{ 154,	220	},		/* "U		*/
-	{ 155,	248	},		/* /o		*/
-	{ 156,	163	},		/* #pound	*/
-	{ 157,	216	},		/* /O		*/
-	{ 158,	215	},		/* #times	*/
-	{ 159,	131	},		/* #f		*/
-	{ 160,	225	},		/* 'a		*/
-	{ 161,	237	},		/* 'i		*/
-	{ 162,	243	},		/* 'o		*/
-	{ 163,	250	},		/* 'u		*/
-	{ 164,	241	},		/* ~n		*/
-	{ 165,	209	},		/* ~N		*/
-	{ 166,	170	},		/* _a		*/
-	{ 167,	186	},		/* _o		*/
-	{ 168,	191	},		/* #?		*/
-	{ 169,	174	},		/* #reg		*/
-	{ 160,	172	},		/* #neg		*/
-	{ 171,	189	},		/* #1/2		*/
-	{ 172,	188	},		/* #1/4		*/
-	{ 173,	161	},		/* #!		*/
-	{ 174,	171	},		/* #<		*/
-	{ 175,	187	},		/* #>		*/
-	{ 176,	0	},
-	{ 177,	0	},
-	{ 178,	0	},
-	{ 179,	0	},
-	{ 180,	0	},
-	{ 181,	193	},		/* 'A		*/
-	{ 182,	194	},		/* ^A		*/
-	{ 183,	192	},		/* `A		*/
-	{ 184,	169	},		/* #copy	*/
-	{ 185,	0	},
-	{ 186,	0	},
-	{ 187,	0	},
-	{ 188,	0	},
-	{ 189,	162	},		/* #cent	*/
-	{ 190,	165	},		/* #yen		*/
-	{ 191,	0	},
-	{ 192,	0	},
-	{ 193,	0	},
-	{ 194,	0	},
-	{ 195,	0	},
-	{ 196,	0	},
-	{ 197,	0	},
-	{ 198,	227	},		/* ~a		*/
-	{ 199,	195	},		/* ~A		*/
-	{ 200,	0	},
-	{ 201,	0	},
-	{ 202,	0	},
-	{ 203,	0	},
-	{ 204,	0	},
-	{ 205,	0	},
-	{ 206,	0	},
-	{ 207,	164	},		/* general currency	*/
-	{ 208,	240	},		/* o|''		*/
-	{ 209,	208	},		/* -D		*/
-	{ 210,	202	},		/* ^E		*/
-	{ 211,	203	},		/* "E		*/
-	{ 212,	200	},		/* `E		*/
-	{ 213,	0	},
-	{ 214,	205	},		/* 'I		*/
-	{ 215,	206	},		/* ^I		*/
-	{ 216,	207	},		/* "I		*/
-	{ 217,	0	},
-	{ 218,	0	},
-	{ 219,	0	},
-	{ 220,	0	},
-	{ 221,	166	},		/* broken dash	*/	/* !!!! */
-	{ 222,	204	},		/* `I		*/
-	{ 223,	0	},
-	{ 224,	211	},		/* 'O		*/
-	{ 225,	223	},		/* "s (beta) */
-	{ 226,	212	},		/* ^O		*/
-	{ 227,	210	},		/* `O		*/
-	{ 228,	245	},		/* ~o		*/
-	{ 229,	213	},		/* ~O		*/
-	{ 230,	181	},		/* #mu		*/
-	{ 231,	222	},		/* |b		*/
-	{ 232,	254	},		/* |B		*/
-	{ 233,	218	},		/* 'U		*/
-	{ 234,	219	},		/* ^U		*/
-	{ 235,	217	},		/* `U		*/
-	{ 236,	253	},		/* 'y		*/
-	{ 237,	221	},		/* 'Y		*/
-	{ 238,	175	},
-	{ 239,	180	},		/* #'		*/
-	{ 240,	173	},		/* strich	*/
-	{ 241,	177	},		/* #pm		*/
-	{ 242,	0	},
-	{ 243,	190	},		/* #3/4		*/
-	{ 244,	182	},		/* #p		*/
-	{ 245,	167	},		/* #sect	*/
-	{ 246,	247	},		/* #div		*/
-	{ 247,	184	},		/* cedille	*/
-	{ 248,	176	},		/* #degree	*/
-	{ 249,	168	},		/* #"		*/
-	{ 250,	149	},		/* #bullet	*/
-	{ 251,	185	},		/* #^1		*/
-	{ 252,	179	},		/* #^3		*/
-	{ 253,	178	},		/* #^2		*/
-	{ 254,	0	},
-	{ 255,	0	},
+   { 128,   199 },                        /* ,C */
+   { 129,   252 },                        /* "u */
+   { 130,   233 },                        /* 'e */
+   { 131,   226 },                        /* ^a */
+   { 132,   228 },                        /* "a */
+   { 133,   224 },                        /* `a */
+   { 134,   229 },                        /* .a */
+   { 135,   231 },                        /* ,c */
+   { 136,   234 },                        /* ^e */
+   { 137,   235 },                        /* "e */
+   { 138,   232 },                        /* `e */
+   { 139,   239 },                        /* "i */
+   { 140,   238 },                        /* ^i */
+   { 141,   236 },                        /* `i */
+   { 142,   196 },                        /* "A */
+   { 143,   197 },                        /* .A */
+   { 144,   201 },                        /* 'E */
+   { 145,   230 },                        /* &ae */
+   { 146,   198 },                        /* &AE */
+   { 147,   244 },                        /* ^o */
+   { 148,   246 },                        /* "o */
+   { 149,   242 },                        /* `o */
+   { 150,   251 },                        /* ^u */
+   { 151,   249 },                        /* `u */
+   { 152,   255 },                        /* "y */
+   { 153,   214 },                        /* "O */
+   { 154,   220 },                        /* "U */
+   { 155,   248 },                        /* /o */
+   { 156,   163 },                        /* #pound */
+   { 157,   216 },                        /* /O */
+   { 158,   215 },                        /* #times */
+   { 159,   131 },                        /* #f */
+   { 160,   225 },                        /* 'a */
+   { 161,   237 },                        /* 'i */
+   { 162,   243 },                        /* 'o */
+   { 163,   250 },                        /* 'u */
+   { 164,   241 },                        /* ~n */
+   { 165,   209 },                        /* ~N */
+   { 166,   170 },                        /* _a */
+   { 167,   186 },                        /* _o */
+   { 168,   191 },                        /* #? */
+   { 169,   174 },                        /* #reg */
+   { 160,   172 },                        /* #neg */
+   { 171,   189 },                        /* #1/2 */
+   { 172,   188 },                        /* #1/4 */
+   { 173,   161 },                        /* #! */
+   { 174,   171 },                        /* #< */
+   { 175,   187 },                        /* #> */
+   { 176,     0 },
+   { 177,     0 },
+   { 178,     0 },
+   { 179,     0 },
+   { 180,     0 },
+   { 181,   193 },                        /* 'A */
+   { 182,   194 },                        /* ^A */
+   { 183,   192 },                        /* `A */
+   { 184,   169 },                        /* #copy */
+   { 185,     0 },
+   { 186,     0 },
+   { 187,     0 },
+   { 188,     0 },
+   { 189,   162 },                        /* #cent */
+   { 190,   165 },                        /* #yen */
+   { 191,     0 },
+   { 192,     0 },
+   { 193,     0 },
+   { 194,     0 },
+   { 195,     0 },
+   { 196,     0 },
+   { 197,     0 },
+   { 198,   227 },                        /* ~a */
+   { 199,   195 },                        /* ~A */
+   { 200,     0 },
+   { 201,     0 },
+   { 202,     0 },
+   { 203,     0 },
+   { 204,     0 },
+   { 205,     0 },
+   { 206,     0 },
+   { 207,   164 },                        /* general currency */
+   { 208,   240 },                        /* o|'' */
+   { 209,   208 },                        /* -D */
+   { 210,   202 },                        /* ^E */
+   { 211,   203 },                        /* "E */
+   { 212,   200 },                        /* `E */
+   { 213,     0 },
+   { 214,   205 },                        /* 'I */
+   { 215,   206 },                        /* ^I */
+   { 216,   207 },                        /* "I */
+   { 217,     0 },
+   { 218,     0 },
+   { 219,     0 },
+   { 220,     0 },
+   { 221,   166 },                        /* broken dash */   /* !!!! */
+   { 222,   204 },                        /* `I */
+   { 223,     0 },
+   { 224,   211 },                        /* 'O */
+   { 225,   223 },                        /* "s (beta) */
+   { 226,   212 },                        /* ^O */
+   { 227,   210 },                        /* `O */
+   { 228,   245 },                        /* ~o */
+   { 229,   213 },                        /* ~O */
+   { 230,   181 },                        /* #mu */
+   { 231,   222 },                        /* |b */
+   { 232,   254 },                        /* |B */
+   { 233,   218 },                        /* 'U */
+   { 234,   219 },                        /* ^U */
+   { 235,   217 },                        /* `U */
+   { 236,   253 },                        /* 'y */
+   { 237,   221 },                        /* 'Y */
+   { 238,   175 },
+   { 239,   180 },                        /* #' */
+   { 240,   173 },                        /* strich */
+   { 241,   177 },                        /* #pm */
+   { 242,     0 },
+   { 243,   190 },                        /* #3/4 */
+   { 244,   182 },                        /* #p */
+   { 245,   167 },                        /* #sect */
+   { 246,   247 },                        /* #div */
+   { 247,   184 },                        /* cedille */
+   { 248,   176 },                        /* #degree */
+   { 249,   168 },                        /* #" */
+   { 250,   149 },                        /* #bullet */
+   { 251,   185 },                        /* #^1 */
+   { 252,   179 },                        /* #^3 */
+   { 253,   178 },                        /* #^2 */
+   { 254,     0 },
+   { 255,     0 }
 };
 
 
-#endif	/* __850ISO__ */
+#endif   /* __850ISO__ */
+
+
+/* +++ EOF +++ */
 
--- Source/ansi2dos.h
+++ Source/ansi2dos.h
@@ -1,177 +1,200 @@
-/*	############################################################
-	# @(#) ansi2dos.h
-	# @(#)
-	# @(#) Copyright (c) 1995-2001 by Dirk Hagedorn
-	# @(#) Dirk Hagedorn (udo@dirk-hagedorn.de)
-	#
-	# This program is free software; you can redistribute it and/or
-	# modify it under the terms of the GNU General Public License
-	# as published by the Free Software Foundation; either version 2
-	# of the License, or (at your option) any later version.
-	# 
-	# This program is distributed in the hope that it will be useful,
-	# but WITHOUT ANY WARRANTY; without even the implied warranty of
-	# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	# GNU General Public License for more details.
-	# 
-	# You should have received a copy of the GNU General Public License
-	# along with this program; if not, write to the Free Software
-	# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-	#
-	#
-	# @(#) Uebersetzungstabellen von Windows-ANSI nach DOS
-	# basierend auf tos2iso.h
-	############################################################	*/
+/**(TAB=0)**********************************************************************
+*
+*  Project name : UDO
+*  Module name  : ansi2dos.h
+*  Symbol prefix: chr
+*
+*  Description  : Uebersetzungstabellen von Windows-ANSI nach DOS
+*                 basierend auf tos2iso.h
+*
+*  Copyright    : 1995-2001 Dirk Hagedorn
+*  Open Source  : since 2001
+*
+*                 This program is free software; you can redistribute it and/or
+*                 modify it under the terms of the GNU General Public License
+*                 as published by the Free Software Foundation; either version 2
+*                 of the License, or (at your option) any later version.
+*                 
+*                 This program is distributed in the hope that it will be useful,
+*                 but WITHOUT ANY WARRANTY; without even the implied warranty of
+*                 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*                 GNU General Public License for more details.
+*                 
+*                 You should have received a copy of the GNU General Public License
+*                 along with this program; if not, write to the Free Software
+*                 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*
+*-------------------------------------------------------------------------------
+*
+*  Author       : Dirk Hagedorn (udo@dirk-hagedorn.de)
+*  Co-Authors   : Ulf Dunkel (fd), Gerhard Stoll (ggs)
+*  Write access : fd, ggs
+*
+*  Notes        : Please add yourself as co-author when you change this file.
+*
+*-------------------------------------------------------------------------------
+*  Things to do : re-write UDO string and encoding engine for full Unicode support 
+*
+*-------------------------------------------------------------------------------
+*  History:
+*
+*  2010:
+*    fd  Feb 12: header updated, file reformatted and tidied up (TAB-free)
+*
+******************************************|************************************/
 
-#ifndef	__ANSI2DOS__
-#define	__ANSI2DOS__
+#ifndef __ANSI2DOS__
+#define __ANSI2DOS__
 
 
 #ifndef ID_ANSI2DOS_H
-#define	ID_ANSI2DOS_H
-const char *id_ansi2dos_h= "@(#) ansi2dos.h  14.10.1996";
+#define ID_ANSI2DOS_H
+const char  *id_ansi2dos_h = "@(#) ansi2dos.h  $date$";
 #endif
 
 
 typedef struct _ansi2dos
 {
-	unsigned char	ansi;
-	unsigned char	dos;
-}	ANSI2DOS;
+   unsigned char   ansi;                  /* */
+   unsigned char   dos;                   /* */
+}  ANSI2DOS;
 
 
-LOCAL const ANSI2DOS ansi2dos_item[128]=
+LOCAL const ANSI2DOS   ansi2dos_item[128] =
 {
-	{ 128, 000 },
-	{ 129, 000 },
-	{ 130, 000 },
-	{ 131, 159 },		/* #f		*/
-	{ 132, 000 },
-	{ 133, 000 },
- 	{ 134, 187 },		/* #dag		*/
-	{ 135, 000 },
-	{ 136, 222 },		/* #circ	*/
-	{ 137, 000 },
-	{ 138, 000 },
-	{ 139, 000 },
-	{ 140, 181 },		/* &OE		*/
-	{ 141, 000 },
-	{ 142, 000 },
-	{ 143, 000 },
-	{ 144, 000 },
-	{ 145, 000 },
-	{ 146, 000 },
-	{ 147, 000 },
-	{ 148, 000 },
-	{ 149, 249 },		/* #bullet	*/
-	{ 150, 000 },
-	{ 151, 000 },
-	{ 152, 000 },
-	{ 153, 191 },		/* #tm		*/
-	{ 154, 000 },
-	{ 155, 000 },
-	{ 156, 180 },		/* &oe		*/
-	{ 157, 000 },
-	{ 158, 000 },
-	{ 159, 000 },
-	{ 160, 000 },
-	{ 161, 173 },		/* #!		*/
-	{ 162, 155 },		/* #cent	*/
-	{ 163, 156 },		/* #pound	*/
-	{ 164, 000 },
-	{ 165, 157 },		/* #yen		*/
-	{ 166, 000 },
-	{ 167, 221 },		/* #sect	*/
-	{ 168, 185 },		/* #"		*/
-	{ 169, 189 },		/* #copy	*/
-	{ 170, 166 },		/* _a		*/
-	{ 171, 174 },		/* #<		*/
-	{ 172, 170 },		/* #neg		*/
-	{ 173, 000 },
-	{ 174, 190 },		/* #reg		*/
-	{ 175, 000 },
-	{ 176, 248 },		/* #degree	*/
-	{ 177, 241 },		/* #pm		*/
-	{ 178, 253 },		/* #^2		*/
-	{ 179, 254 },		/* #^3		*/
-	{ 180, 186 },		/* #'		*/
-	{ 181, 230 },		/* #mu		*/
-	{ 182, 188 },		/* #p		*/
-	{ 183, 250 },		/* #cdot	*/
-	{ 184, 000 },
-	{ 185, 000 },
-	{ 186, 167 },		/* _o		*/
-	{ 187, 175 },		/* #>		*/
-	{ 188, 172 },		/* #1/4		*/
-	{ 189, 171 },		/* #1/2		*/
-	{ 190, 000 },
-	{ 191, 168 },		/* #?		*/
-	{ 192, 182 },		/* `A		*/
-	{ 193, 000 },
-	{ 194, 000 },
-	{ 195, 183 },		/* ~A		*/
-	{ 196, 142 },		/* "A		*/
-	{ 197, 143 },		/* .A		*/
-	{ 198, 146 },		/* &AE		*/
-	{ 199, 128 },		/* ,C		*/
-	{ 200, 000 },
-	{ 201, 144 },		/* 'E		*/
-	{ 202, 000 },
-	{ 203, 000 },
-	{ 204, 000 },
-	{ 205, 000 },
-	{ 206, 000 },
-	{ 207, 000 },
-	{ 208, 000 },
-	{ 209, 165 },		/* ~N		*/
-	{ 210, 000 },
-	{ 211, 000 },
-	{ 212, 000 },
-	{ 213, 184 },		/* ~O		*/
-	{ 214, 153 },		/* "O		*/
-	{ 215, 000 },
-	{ 216, 178 },		/* /O		*/
-	{ 217, 000 },
-	{ 218, 000 },
-	{ 219, 000 },
-	{ 220, 154 },		/* "U		*/
-	{ 221, 000 },
-	{ 222, 000 },
-	{ 223, 225 },		/* "s (sz)	*/
-	{ 224, 133 },		/* `a		*/
-	{ 225, 160 },		/* 'a		*/
-	{ 226, 131 },		/* ^a		*/
-	{ 227, 176 },		/* ~a		*/
-	{ 228, 132 },		/* "a		*/
-	{ 229, 134 },		/* .a		*/
-	{ 230, 145 },		/* &ae		*/
-	{ 231, 135 },		/* ,c		*/
-	{ 232, 138 },		/* `e		*/
-	{ 233, 130 },		/* 'e		*/
-	{ 234, 136 },		/* ^e		*/
-	{ 235, 137 },		/* "e		*/
-	{ 236, 141 },		/* `i		*/
-	{ 237, 161 },		/* 'i		*/
-	{ 238, 140 },		/* ^i		*/
-	{ 239, 139 },		/* "i		*/
-	{ 240, 000 },
-	{ 241, 164 },		/* ~n		*/
-	{ 242, 149 },		/* `o		*/
-	{ 243, 162 },		/* 'o		*/
-	{ 244, 147 },		/* ^o		*/
-	{ 245, 177 },		/* ~o		*/
-	{ 246, 148 },		/* "o		*/
-	{ 247, 246 },		/* div		*/
-	{ 248, 179 },		/* /o		*/
-	{ 249, 151 },		/* `u		*/
-	{ 250, 163 },		/* 'u		*/
-	{ 251, 150 },		/* ^u		*/
-	{ 252, 129 },		/* "u		*/
-	{ 253, 000 },
-	{ 254, 000 },
-	{ 255, 152 },		/* "y		*/
+   { 128,   0 },
+   { 129,   0 },
+   { 130,   0 },
+   { 131, 159 },                          /* #f */
+   { 132,   0 },
+   { 133,   0 },
+   { 134, 187 },                          /* #dag */
+   { 135,   0 },
+   { 136, 222 },                          /* #circ */
+   { 137,   0 },
+   { 138,   0 },
+   { 139,   0 },
+   { 140, 181 },                          /* &OE */
+   { 141,   0 },
+   { 142,   0 },
+   { 143,   0 },
+   { 144,   0 },
+   { 145,   0 },
+   { 146,   0 },
+   { 147,   0 },
+   { 148,   0 },
+   { 149, 249 },                          /* #bullet */
+   { 150,   0 },
+   { 151,   0 },
+   { 152,   0 },
+   { 153, 191 },                          /* #tm */
+   { 154,   0 },
+   { 155,   0 },
+   { 156, 180 },                          /* &oe */
+   { 157,   0 },
+   { 158,   0 },
+   { 159,   0 },
+   { 160,   0 },
+   { 161, 173 },                          /* #! */
+   { 162, 155 },                          /* #cent */
+   { 163, 156 },                          /* #pound */
+   { 164,   0 },
+   { 165, 157 },                          /* #yen */
+   { 166,   0 },
+   { 167, 221 },                          /* #sect */
+   { 168, 185 },                          /* #" */
+   { 169, 189 },                          /* #copy */
+   { 170, 166 },                          /* _a */
+   { 171, 174 },                          /* #< */
+   { 172, 170 },                          /* #neg */
+   { 173,   0 },
+   { 174, 190 },                          /* #reg */
+   { 175,   0 },
+   { 176, 248 },                          /* #degree */
+   { 177, 241 },                          /* #pm */
+   { 178, 253 },                          /* #^2 */
+   { 179, 254 },                          /* #^3 */
+   { 180, 186 },                          /* #' */
+   { 181, 230 },                          /* #mu */
+   { 182, 188 },                          /* #p */
+   { 183, 250 },                          /* #cdot */
+   { 184,   0 },
+   { 185,   0 },
+   { 186, 167 },                          /* _o */
+   { 187, 175 },                          /* #> */
+   { 188, 172 },                          /* #1/4 */
+   { 189, 171 },                          /* #1/2 */
+   { 190,   0 },
+   { 191, 168 },                          /* #? */
+   { 192, 182 },                          /* `A */
+   { 193,   0 },
+   { 194,   0 },
+   { 195, 183 },                          /* ~A */
+   { 196, 142 },                          /* "A */
+   { 197, 143 },                          /* .A */
+   { 198, 146 },                          /* &AE */
+   { 199, 128 },                          /* ,C */
+   { 200,   0 },
+   { 201, 144 },                          /* 'E */
+   { 202,   0 },
+   { 203,   0 },
+   { 204,   0 },
+   { 205,   0 },
+   { 206,   0 },
+   { 207,   0 },
+   { 208,   0 },
+   { 209, 165 },                          /* ~N */
+   { 210,   0 },
+   { 211,   0 },
+   { 212,   0 },
+   { 213, 184 },                          /* ~O */
+   { 214, 153 },                          /* "O */
+   { 215,   0 },
+   { 216, 178 },                          /* /O */
+   { 217,   0 },
+   { 218,   0 },
+   { 219,   0 },
+   { 220, 154 },                          /* "U */
+   { 221,   0 },
+   { 222,   0 },
+   { 223, 225 },                          /* "s (sz) */
+   { 224, 133 },                          /* `a */
+   { 225, 160 },                          /* 'a */
+   { 226, 131 },                          /* ^a */
+   { 227, 176 },                          /* ~a */
+   { 228, 132 },                          /* "a */
+   { 229, 134 },                          /* .a */
+   { 230, 145 },                          /* &ae */
+   { 231, 135 },                          /* ,c */
+   { 232, 138 },                          /* `e */
+   { 233, 130 },                          /* 'e */
+   { 234, 136 },                          /* ^e */
+   { 235, 137 },                          /* "e */
+   { 236, 141 },                          /* `i */
+   { 237, 161 },                          /* 'i */
+   { 238, 140 },                          /* ^i */
+   { 239, 139 },                          /* "i */
+   { 240,   0 },
+   { 241, 164 },                          /* ~n */
+   { 242, 149 },                          /* `o */
+   { 243, 162 },                          /* 'o */
+   { 244, 147 },                          /* ^o */
+   { 245, 177 },                          /* ~o */
+   { 246, 148 },                          /* "o */
+   { 247, 246 },                          /* div */
+   { 248, 179 },                          /* /o */
+   { 249, 151 },                          /* `u */
+   { 250, 163 },                          /* 'u */
+   { 251, 150 },                          /* ^u */
+   { 252, 129 },                          /* "u */
+   { 253,   0 },
+   { 254,   0 },
+   { 255, 152 }                           /* "y */
 };
 
 
-#endif	/* __ANSI2DOS__ */
+#endif   /* __ANSI2DOS__ */
+
+
+/* +++ EOF +++ */
 
