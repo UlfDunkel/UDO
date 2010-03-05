f8c6ee53661a5e7c89d0e8d2486e57aa4fcaeb34
ulf_svn <6141297+UlfDunkel@users.noreply.github.com>
2010-03-05 07:17:49 +0000

    - file tidied up - message wrapper functions now global

--- Source/msg.h
+++ Source/msg.h
@@ -39,38 +39,55 @@
 *
 *  2010:
 *    fd  Feb 22: - header adjusted
-                 - VOID, SBYTE, UBYTE, SWORD, UWORD, SLONG, ULONG introduced
+*                - VOID, SBYTE, UBYTE, SWORD, UWORD, SLONG, ULONG introduced
+*    fd  Mar 04: - file tidied up
+*                - message wrapper functions now global
 *
 ******************************************|************************************/
 
-/*   ######################################################################
-   # Globale Variablen
-   ######################################################################   */
+/*******************************************************************************
+*
+*     GLOBAL PROTOTYPES
+*
+******************************************|************************************/
+
+   /* --- message wrapper functions --- */
+
+GLOBAL void fatal_msg_solo(const char *s);
+GLOBAL void fatal_msg_para(const char *f, const char *s);
+
+GLOBAL void error_msg_solo(const char *s);
+GLOBAL void error_msg_para(const char *f, const char *s);
+
+GLOBAL void warning_msg_solo(const char *em);
+GLOBAL void warning_msg_para(const char *f, const char *s);
+
+GLOBAL void note_msg_solo(const char *em);
+GLOBAL void note_msg_para(const char *f, const char *s);
 
 
-/*   ######################################################################
-   # Ausgabe von Strings ins Logfile
-   ######################################################################   */
+
+   /* --- string output to log file --- */
+
 GLOBAL void logln(const char *s);
 GLOBAL void vloglnf ( const char *fmt, ... ) __attribute__((format(printf, 1, 2)));
 
 
-/*   ######################################################################
-   # Informationen, die ins Logfile geschrieben werden
-   ######################################################################   */
+   /* --- write information into log file --- */
+
 GLOBAL void logln_warnings_errors(void);
 GLOBAL void logln_information(void);
 GLOBAL void logln_interrupted(void);
-GLOBAL void logln_file_generated ( const char *kind, const char *filename, const char *suff );
+GLOBAL void logln_file_generated(const char *kind, const char *filename, const char *suff );
 
-GLOBAL void error_message ( const char *msg );
-GLOBAL void warning_message ( const char *msg );
-GLOBAL void note_message ( const char *msg );
-GLOBAL void fatal_message ( const char *msg );
+GLOBAL void error_message(const char *msg );
+GLOBAL void warning_message(const char *msg );
+GLOBAL void note_message(const char *msg );
+GLOBAL void fatal_message(const char *msg );
 
-/*   ######################################################################
-   # Programm-Fehlermeldungen
-   ######################################################################   */
+
+   /* --- program error messages --- */
+
 GLOBAL void error_open_logfile(const char *s);
 GLOBAL void error_open_hypfile(const char *s);
 GLOBAL void error_open_idxfile(const char *s);
@@ -95,20 +112,19 @@ GLOBAL void error_buffer_overflow(void);
 GLOBAL void error_empty_docinfo(void);
 GLOBAL void error_missing_title_data(void);
 GLOBAL void error_unknown_docinfo(const char *s);
-GLOBAL void error_unknown_html_navigation ( const char *s );  /* New V 6.5.20 */
+GLOBAL void error_unknown_html_navigation(const char *s);  /* New V 6.5.20 */
 GLOBAL void error_unknown_color(const char *s);
-GLOBAL void error_wrong_header_date ( const char *s ); /* New in V6.5.5 [NHz] */
-GLOBAL void error_empty_header_links ( void );   /* New feature #0000053 in V6.5.2 [NHz] */
-GLOBAL void error_argument_header_links ( const char *s ); /* New feature #0000053 in V6.5.2 [NHz] */
+GLOBAL void error_wrong_header_date(const char *s); /* New in V6.5.5 [NHz] */
+GLOBAL void error_empty_header_links(void);   /* New feature #0000053 in V6.5.2 [NHz] */
+GLOBAL void error_argument_header_links(const char *s); /* New feature #0000053 in V6.5.2 [NHz] */
 GLOBAL void error_error(const char *s);
 
 
-/*   ######################################################################
-   # grobe Syntaxfehler
-   ######################################################################   */
+   /* --- severe syntax errors --- */
+
 GLOBAL void error_syntax_error(void);
-GLOBAL void error_end_without_begin ( const char *se, const char *sb );
-GLOBAL void error_wrong_end ( const char *sb, const char *se );
+GLOBAL void error_end_without_begin(const char *se, const char *sb);
+GLOBAL void error_wrong_end(const char *sb, const char *se);
 GLOBAL void error_item_outside_env(void);
 GLOBAL void error_item_many_enum(void);
 GLOBAL void error_missing_end(const char *s);
@@ -124,11 +140,11 @@ GLOBAL void error_unknown_command(const char *s);
 GLOBAL void error_unexpected_eol(void);
 GLOBAL void error_wrong_nr_parameters(const char *s);
 GLOBAL void error_too_many_macros(void);
-GLOBAL void error_long_macro_name ( int len );
-GLOBAL void error_long_macro_cont ( int len );
+GLOBAL void error_long_macro_name(int len);
+GLOBAL void error_long_macro_cont(int len);
 GLOBAL void error_too_many_defines(void);
-GLOBAL void error_long_define_name ( int len );
-GLOBAL void error_long_define_cont ( int len );
+GLOBAL void error_long_define_name(int len);
+GLOBAL void error_long_define_cont(int len);
 GLOBAL void error_too_many_placeholder(void);
 GLOBAL void error_too_many_symbols(void);
 GLOBAL void error_unset_symbol(const char *s);
@@ -139,11 +155,11 @@ GLOBAL void error_too_many_label(void);
 GLOBAL void error_too_many_alias(void);
 GLOBAL void error_too_many_files(void);
 GLOBAL void error_replace_param(const char *s);
-GLOBAL void error_undefined_link ( const char *l );
-GLOBAL void error_too_many_if ( const char *s, const UWORD l );
+GLOBAL void error_undefined_link(const char *l);
+GLOBAL void error_too_many_if(const char *s, const UWORD l);
 GLOBAL void error_else_without_if(void);
 GLOBAL void error_endif_without_if(void);
-GLOBAL void error_missing_endif ( const char *s, const UWORD l );
+GLOBAL void error_missing_endif(const char *s, const UWORD l);
 GLOBAL void error_table_width(void);
 GLOBAL void error_table_height(void);
 GLOBAL void error_table_cell_width(void);
@@ -164,40 +180,45 @@ GLOBAL void error_node3_not_allowed(void);
 GLOBAL void error_node4_not_allowed(void);
 GLOBAL void error_node5_not_allowed(void);
 
-/*   ######################################################################
-   # Warnmeldungen (leichte Syntaxfehler)
-   ######################################################################   */
+
+   /* --- warnings (tiny syntax errors) --- */
+
 GLOBAL void note_long_sourceline(void);
-GLOBAL void warning_long_destline ( const char *s, const UWORD lnr, const int ll);
+GLOBAL void warning_long_destline(const char *s, const UWORD lnr, const int ll);
 
 GLOBAL void note_short_sourceline(const char *s);
-GLOBAL void warning_short_destline ( const char *s, const UWORD lnr, const int ll, const char *w);
+GLOBAL void warning_short_destline(const char *s, const UWORD lnr, const int ll, const char *w);
 
 GLOBAL void warning_no_unichar(const char *s);
-GLOBAL void warning_cannot_recode ( const char c, const char *scs, const char *dcs );
-GLOBAL void warning_cannot_recode_utf8 ( const char *utf, const char *dcs );
-GLOBAL void warning_no_isochar ( const char c );
-GLOBAL void warning_no_texchar ( const char c );
+GLOBAL void warning_cannot_recode(const char c, const char *scs, const char *dcs);
+GLOBAL void warning_cannot_recode_utf8(const char *utf, const char *dcs);
+GLOBAL void warning_no_isochar(const char c);
+GLOBAL void warning_no_texchar(const char c);
 
 GLOBAL void warning_split_verb(void);
 
 GLOBAL void warning_node_too_deep(void);
 
-/*   ######################################################################
-   # Abfrageroutinen fuer andere Routinen
-   ######################################################################   */
+
+   /* --- inquiry functions --- */
+
 GLOBAL int get_warning_counter(void);
 GLOBAL int get_note_counter(void);
 GLOBAL int get_error_counter(void);
 
-/*   ######################################################################
-   # Modulinit
-   ######################################################################   */
+
+   /* --- module initialization --- */
+
 GLOBAL void init_module_msg(void);
 
-/* define this, to let loglnposprintf do printf, instead passing string to gui */
+
+
+   /* --- debug functions --- */
+   
+/* define this, to let loglnposprintf do printf, instead passing string to GUI */
 #define USE_LOGLNPOSPRINTF
-GLOBAL void loglnposprintf ( const char *we, const char *msg );
+
+GLOBAL void loglnposprintf(const char *we, const char *msg);
 
 
 /* +++ EOF +++ */
