a9b429f7bdce2c43511799bc16dceaff7fcda56d
ulf_svn <6141297+UlfDunkel@users.noreply.github.com>
2010-05-19 11:04:53 +0000

    files tidied up

--- Source/par.h
+++ Source/par.h
@@ -39,126 +39,168 @@
 *
 *  2010:
 *    fd  Feb 22: - header adjusted
-                 - VOID, SBYTE, UBYTE, SWORD, UWORD, SLONG, ULONG introduced
+*                - VOID, SBYTE, UBYTE, SWORD, UWORD, SLONG, ULONG introduced
+*    fd  May 19: file tidied up
 *
 ******************************************|************************************/
 
-/*   ############################################################
-   # Typen
-   ############################################################   */
 #ifndef UDO_PAR_H
 #define UDO_PAR_H
 
-/*   --------------------------------------------------
-   Trennvorschlaege
-   --------------------------------------------------   */
-#define   HYPHEN_LEN   128
-
-typedef struct _hyphen
-{
-   char   hyphen[HYPHEN_LEN+1];   /* Wort mit Trennstrichen drin   */
-   char   solo[HYPHEN_LEN+1];      /* gleiches Wort, aber ohne !-   */
-}   HYPHEN, *pHYPHEN;
 
 
-/*   --------------------------------------------------
-   Makros
-   --------------------------------------------------   */
-#define   MACRO_NAME_LEN   32
-#define   MACRO_CONT_LEN   256
-
-typedef struct _macros
-{
-   char   name[MACRO_NAME_LEN+1];      /* Der Name des Makros      */
-   char   entry[MACRO_CONT_LEN+1];   /* Der Inhalt des Makros   */
-   BOOLEAN   vars;                  /* mit Parametern? PL3      */
-}   MACROS, *pMACROS;
 
 
-/*   --------------------------------------------------
-   Definitionen
-   --------------------------------------------------   */
-#define   DEFINE_NAME_LEN   32
-#define   DEFINE_CONT_LEN   256
+/*******************************************************************************
+*
+*     MACRO DEFINITIONS
+*
+******************************************|************************************/
 
-typedef struct _defs
-{
-   char name[DEFINE_NAME_LEN+1];      /* Der Name der Definition   */
-   char entry[DEFINE_CONT_LEN+1];      /* Der Inhalt            */
-   BOOLEAN   vars;                  /* mit Parametern? PL3      */
-}   DEFS, *pDEFS;
-
-
-/*   --------------------------------------------------
-   Allgemeiner Platzhalter
-   --------------------------------------------------   */
-typedef struct _placeholder            
-{
-   char   magic[10];   /* Eine Steuermarke <ESC><0xB0+nr>   */
-   char   *entry;      /* Das komplette Kommando         */
-   char   *text;      /* Nur Text (wichtig fuer toklen())   */
-   BOOLEAN   replaced;   /* TRUE: PH ist bereits ersetzt worden */
-}   PLACEHOLDER;
+#define HYPHEN_LEN        128             /* length of a word */
+#define MACRO_NAME_LEN     32             /* length of a macro name */
+#define MACRO_CONT_LEN    256             /* length of a macro content */
+#define DEFINE_NAME_LEN    32             /* length of a definition name */
+#define DEFINE_CONT_LEN   256             /* length of a definition content */
 
 
-/*   --------------------------------------------------
-   Spezielle Platzhalter fuer Formatbefehle
-   --------------------------------------------------   */
-typedef struct _speccmd
-{
-   char   magic[6];   /* Eine Steuermarke <ESC><0xB0+nr>   */
-   char   *entry;      /* Das komplette Kommando         */
-   BOOLEAN   replaced;   /* TRUE: PH ist bereits ersetzt worden */
-}   SPECCMD;
+
+
+
+/*******************************************************************************
+*
+*     TYPE DEFINITIONS
+*
+******************************************|************************************/
+
+typedef struct _hyphen                    /* hyphenated word */
+   {
+   char   hyphen[HYPHEN_LEN + 1];         /* word with hyphen marks */
+   char   solo[  HYPHEN_LEN + 1];         /* same word, but without !- */
+   } HYPHEN, *pHYPHEN;
+
+
+typedef struct _macros                    /* !macro structur */
+   {
+   char      name[ MACRO_NAME_LEN + 1];   /* macro name */
+   char      entry[MACRO_CONT_LEN + 1];   /* macro content */
+   BOOLEAN   vars;                        /* optional parameters */
+   } MACROS, *pMACROS;
+
+
+typedef struct _defs                      /* !define structure */
+   {
+   char      name[ DEFINE_NAME_LEN + 1];  /* definition name, formatted as (!%s) */
+   char      entry[DEFINE_CONT_LEN + 1];  /* definition content */
+   BOOLEAN   vars;                        /* optional parameters */
+   } DEFS, *pDEFS;
+
+typedef struct _placeholder               /* general placeholder */
+   {
+   char      magic[10];                   /* a control magic <ESC><0xB0 + nr> */
+   char     *entry;                       /* the whole command */
+   char     *text;                        /* text only (required by toklen()) */
+   BOOLEAN   replaced;                    /* TRUE: PH has already been replaced */
+   } PLACEHOLDER;
+
+
+typedef struct _speccmd                   /* special format command placeholder */
+   {
+   char      magic[6];                    /* a control magic <ESC><0xB0 + nr> */
+   char     *entry;                       /* the whole command */
+   BOOLEAN   replaced;                    /* TRUE: PH has already been replaced */
+   } SPECCMD;
 
 #endif   /* UDO_PAR_H */
 
 
 
-/*   ############################################################
-   # globale Variablen
-   ############################################################   */
-GLOBAL UWORD   hyphen_counter;      /* Anzahl von geladenen Trennregeln      */
-GLOBAL UWORD   macro_counter;      /* Anzahl geladener Makros            */
-GLOBAL UWORD   define_counter;      /* Anzahl geladener defines            */
-
-GLOBAL PLACEHOLDER   phold[MAXPHOLDS + 1]; /* Platzhalter */
-GLOBAL int           phold_counter;
 
 
-/*   ############################################################
-   # globale Funktionen
-   ############################################################   */
-GLOBAL void replace_variables ( char *s, const char *cmd, const char *entry);
+/*******************************************************************************
+*
+*     GLOBAL VARIABLES
+*
+******************************************|************************************/
 
+GLOBAL UWORD         hyphen_counter;      /* # of loaded hyphenation rules */
+GLOBAL UWORD         macro_counter;       /* # of loaded macros */
+GLOBAL UWORD         define_counter;      /* # of loaded definitions */
+
+GLOBAL PLACEHOLDER   phold[MAXPHOLDS + 1];/* array of placeholders */
+GLOBAL int           phold_counter;       /* # of placeholders */
+
+
+
+
+
+/*******************************************************************************
+*
+*     GLOBAL PROTOTYPES
+*
+******************************************|************************************/
+
+   /* adjust macros and definitions which contain variables */
+GLOBAL void replace_variables(char *s, const char *cmd, const char *entry);
+
+   /* remove special format command placeholders from the text */
 GLOBAL void reset_speccmds(void);
-GLOBAL BOOLEAN add_speccmd ( char *entry );
-GLOBAL BOOLEAN insert_speccmd ( char *s, const char *rep, char *entry );
+
+   /* insert special format commands into the text as placeholders */
+GLOBAL BOOLEAN add_speccmd(char *entry);
+
+   /* insert special format command placeholders into the text */
+GLOBAL BOOLEAN insert_speccmd(char *s, const char *rep, char *entry);
+
+   /* replace special format command placeholders in the text */
 GLOBAL void replace_speccmds(char *s);
 
-GLOBAL BOOLEAN add_placeholder ( char *entry, char *rawtext );
-GLOBAL BOOLEAN insert_placeholder ( char *s, const char *rep, char *entry, char *rawtext );
-GLOBAL void replace_placeholders(char *s);
-GLOBAL void replace_placeholders_text(char *s);
+   /* handle links and other internal commands */
 GLOBAL void reset_placeholders(void);
-GLOBAL size_t pholdlen ( int i );
 
-GLOBAL void c_internal_index ( char *s, const BOOLEAN inside_b4_macro);
+   /*  */
+GLOBAL BOOLEAN add_placeholder(char *entry, char *rawtext);
 
-GLOBAL void c_commands_inside ( char *s, const BOOLEAN inside_b4_macro );
+   /*  */
+GLOBAL BOOLEAN insert_placeholder(char *s, const char *rep, char *entry, char *rawtext);
 
+   /* replace the internal placeholder commands by human-readable content */
+GLOBAL void replace_placeholders(char *s);
 
+   /*  */
+GLOBAL void replace_placeholders_text(char *s);
+
+   /* return length of placeholder text */
+GLOBAL size_t pholdlen(int i);
+
+   /*  */
+GLOBAL void c_internal_index(char *s, const BOOLEAN inside_b4_macro);
+
+   /*  */
+GLOBAL void c_commands_inside(char *s, const BOOLEAN inside_b4_macro);
+
+   /* replace word with hyphenated word in document */
 GLOBAL void replace_hyphens(char *s);
+
+   /* add hyphen to list of hyphens */
 GLOBAL void add_hyphen(void);
 
+   /* replace macro (!%s) with macro content in document */
 GLOBAL void replace_macros(char *s);
+
+   /* add macro to list of macros */
 GLOBAL BOOLEAN add_macro(void);
 
+   /* replace definition (!%s) with definition content in document */
 GLOBAL void replace_defines(char *s);
+
+   /* add definition to list of definitions */
 GLOBAL BOOLEAN add_define(void);
 
-
+   /* initialize all arrays used in this module */
 GLOBAL void init_module_par(void);
+
+   /* release all arrays used in this module */
 GLOBAL void exit_module_par(void);
 
 
