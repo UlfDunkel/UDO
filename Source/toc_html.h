030b0f53c61bee58cc5884061116c2579c948f20
ulf_svn <6141297+UlfDunkel@users.noreply.github.com>
2011-02-14 13:42:01 +0000

    all set_html_...() functions moved from TOC to TOC_HTML

--- Source/toc_html.h
+++ Source/toc_html.h
@@ -39,6 +39,8 @@
 *
 *  2010:
 *    fd  Jan 23: file reformatted
+*  2011:
+*    fd  Feb 14: all set_html_...() functions moved from TOC to TOC_HTML
 *
 ******************************************|************************************/
 
@@ -48,7 +50,51 @@
 *
 ******************************************|************************************/
 
-   /* Creates the navigation bar for HTML output. */
+   /* creates the navigation bar for HTML output */
 GLOBAL BOOLEAN set_html_navigation(void);
+
+   /* sets the variable html_doctype */
+GLOBAL void set_html_doctype(void);
+
+   /* sets HTML header date */
+GLOBAL void set_html_header_date(void);
+
+   /*  */
+GLOBAL void set_html_header_links(void);
+
+   /* sets various html_frames_... flags */
+GLOBAL void set_html_frames_layout(void);
+
+GLOBAL void set_html_filename(void);
+GLOBAL void set_html_filename_prefix(void);
+GLOBAL void set_html_dirname(void);
+GLOBAL void set_html_switch_language(void);
+
+GLOBAL void set_html_keywords(void);
+GLOBAL void set_html_description(void);
+GLOBAL void set_html_robots(void);        /* new V6.5.17 */
+GLOBAL void set_html_bgsound(void);       /* New in 6.5.20 [GS] */
+GLOBAL void set_html_backimage(void);
+GLOBAL void set_html_color(const int which);
+
+GLOBAL void set_html_style(void);
+GLOBAL void set_html_script(void);
+GLOBAL void set_html_favicon(void);
+GLOBAL void set_html_special_color(char *hc);
+GLOBAL void set_html_counter_command(void);
+
+GLOBAL void set_html_frames_toc_title(void); /* New in 6.5.16 [GS] */
+GLOBAL void set_html_frames_con_title(void); /* New in 6.5.16 [GS] */
+
+GLOBAL void set_html_modern_width(void);
+GLOBAL void set_html_modern_alignment(void);
+GLOBAL void set_html_modern_backimage(void);
+GLOBAL void set_html_frames_width(void);
+GLOBAL void set_html_frames_height(void);
+GLOBAL void set_html_frames_alignment(void);
+GLOBAL void set_html_frames_position(void);
+GLOBAL void set_html_frames_backimage(void);
+GLOBAL void set_html_button_alignment(void);
+GLOBAL void set_html_quotes(void);
 
 /* +++ EOF +++ */
