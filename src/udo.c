ac9268452748864a010b8ea68737f63ca1c55a9d
Thorsten Otto <admin@tho-otto.de>
2016-04-03 18:19:13 +0200

    do not recode raw environments

--- src/udo.c
+++ src/udo.c
@@ -10119,7 +10119,7 @@ LOCAL _BOOL pass1(const char *datei)
       }
       
       /* Kommentare nicht recoden */
-      if (zeile[0] != '#' && pflag[PASS1].ignore_line == 0)
+      if (zeile[0] != '#' && pflag[PASS1].ignore_line == 0 && pflag[PASS1].env != ENV_RAW)
       {
          recode(zeile, iEncodingSource, iEncodingTarget);
       }
@@ -11461,7 +11461,7 @@ LOCAL _BOOL pass2(const char *datei)
          len--;
       }
 
-      if (zeile[0] != EOS && zeile[0] != '#' && pflag[PASS2].ignore_line == 0)
+      if (zeile[0] != EOS && zeile[0] != '#' && pflag[PASS2].ignore_line == 0 && pflag[PASS2].env != ENV_RAW)
       {
          recode(zeile, iEncodingSource, iEncodingTarget);
       }
